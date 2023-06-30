9990d495a69fa9277b1345ece61d9452a7871360
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-06-30 23:55:28 +0300

    Update SOLUTION.sql

---
+++ lesson9/src/main/resources/SOLUTION.sql
@@ -0,0 +1,18 @@
+SELECT student.id, name 
+FROM student 
+	JOIN mark ON student.id = mark.student_id 
+GROUP BY student.id, name
+HAVING AVG(mark.mark) > 8;
+
+SELECT student.id, name 
+FROM student 
+	JOIN mark ON student.id = mark.student_id 
+GROUP BY student.id, name
+HAVING MIN(mark.mark) > 7;
+
+SELECT s.id, s.name 
+FROM student AS s 
+	JOIN payment AS p ON s.id = p.student_id 
+WHERE EXTRACT(YEAR FROM p.payment_date) = 2019 
+GROUP BY s.id, s.name
+HAVING COUNT(*) > 2;

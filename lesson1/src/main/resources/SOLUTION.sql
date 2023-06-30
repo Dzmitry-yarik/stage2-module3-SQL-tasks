8c1f662fa51a8aed459f113fee005c87c364e3fc
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-06-30 20:33:39 +0300

    Update SOLUTION.sql

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -23,7 +23,7 @@ CREATE TABLE payment
 (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     type_id BIGINT NOT NULL,
-    amount DECIMAL(2) NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
     payment_date TIMESTAMP  NOT NULL,
     student_id BIGINT NOT NULL,
     foreign key (type_id) references paymenttype(id),

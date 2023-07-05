cbc27056355506f47b5f77151529018adfcd3012
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-07-05 17:08:23 +0300

    Update SOLUTION.sql

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,6 +1,7 @@
-SELECT * FROM payment WHERE amount >= 500;
-SELECT * FROM student WHERE DATEADD(now(), INTERVAL -20 YEAR) > birthday;
-SELECT * FROM student WHERE name LIKE 'Mike%' OR groupnumber IN (4, 5, 6);
-SELECT * FROM payment WHERE DATEADD(now(), INTERVAL -8 MONTH) < payment_date;
-SELECT * FROM student WHERE name LIKE 'A%';
-SELECT * FROM student WHERE (name LIKE 'Roxi%' AND groupnumber = 4) OR (name LIKE 'Tallie%' AND groupnumber = 9);
+SELECT * FROM Payment WHERE amount >= 500;
+SELECT * FROM Student WHERE DATEADD(YEAR, -20, CURRENT_DATE) > birthday;
+SELECT * FROM Student WHERE groupnumber = 10 AND DATEADD(YEAR, -20, CURRENT_DATE) < birthday;
+SELECT * FROM Student WHERE name LIKE 'Mike%' OR groupnumber IN (4, 5, 6);
+SELECT * FROM Payment WHERE DATEADD(MONTH, -8, CURRENT_DATE) < payment_date;
+SELECT * FROM Student WHERE name LIKE 'A%';
+SELECT * FROM Student WHERE (name LIKE 'Roxi%' AND groupnumber = 4) OR (name LIKE 'Tallie%' AND groupnumber = 9);

8dbf515098393e70c7b5a4731ffdd48dbe2ed782
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-07-05 16:18:28 +0300

    Update CREATEDB.sql

--- lesson3/src/main/resources/CREATEDB.sql
+++ lesson3/src/main/resources/CREATEDB.sql
@@ -1,42 +1,42 @@
 CREATE TABLE student
 (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(45),
-    birthday DATE,
+    name VARCHAR(50),
+    birthday DATE NOT NULL,
     groupnumber INT NOT NULL
 );
 CREATE TABLE subject
 (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(250),
+    name VARCHAR(50),
     description VARCHAR(255),
-    grade int NOT NULL
-);
-
-CREATE TABLE mark
-(
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    student_id BIGINT,
-    subject_id BIGINT,
-    mark INT,
-    foreign key (student_id) references student(id),
-    foreign key (subject_id) references subject(id)
+    grade int NOT NULL CHECK(mark >= 1 AND mark <= 5)
 );
 
 CREATE TABLE paymenttype
 (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(45)
+    name VARCHAR(50) UNIQUE
 );
 
 CREATE TABLE payment
 (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    type_id BIGINT,
-    amount decimal,
-    payment_date DateTime,
+    type_id BIGINT NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    payment_date TIMESTAMP  NOT NULL,
     student_id BIGINT NOT NULL,
     foreign key (type_id) references paymenttype(id),
     foreign key (student_id) references student(id)
+);
+
+CREATE TABLE mark
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    student_id BIGINT NOT NULL,
+    subject_id BIGINT NOT NULL,
+    mark INT NOT NULL CHECK(mark >= 1 AND mark <= 10),
+    foreign key (student_id) references student(id),
+    foreign key (subject_id) references subject(id)
 );
 
